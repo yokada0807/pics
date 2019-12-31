0792248f07a665dc56de2f00c93f750527e1d939
Yoshihisa Okada <yoshi19920807@gmail.com>
2020-01-01 06:57:43 +0900

    no message

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  state = {term: '' };
+
+  // onIinputChange(event) {
+  //   console.log(event.target.value)
+  // }
+
+  // onInputClick() {
+  //   console.log('Input was clicked')
+  // }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+              // argument is input instead of ()
+              // onClick={this.onInputClick}
+            />         
+            {/*  */}
           </div>
         </form>
       </div>
